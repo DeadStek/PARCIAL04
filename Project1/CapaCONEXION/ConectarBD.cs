6cbb53cd3e9ab240b0004d4c51dbefd54a450621
DeadStek <48686765+DeadStek@users.noreply.github.com>
2019-05-21 20:05:22 -0600

    Add files via upload
    Diego Armando Valle Rodriguez   25 - 24 79 - 2015
    Gisella Margarita Ortiz Terezon    25 - 7007 - 2014

---
+++ Project1/CapaCONEXION/ConectarBD.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace CapaCONEXION
+{
+    public class ConectarBD
+    {
+        
+        static SqlDataAdapter Adaptador = new SqlDataAdapter();
+        static SqlConnection Conectar = new SqlConnection();
+        static SqlCommandBuilder GeneradorComandos;
+        static SqlCommand Comando = new SqlCommand();
+
+        String cadenaConex = " Data Source=.;Initial Catalog=hospital;User ID=gisella_ortiz;Password=123";
+
+        //VARIABLE PRIVADA
+        private string consultasSQL;
+
+        public string ConsultasSQL
+        {
+            get { return consultasSQL; }
+            set { consultasSQL = value; }
+        }
+
+        //METODO DE CONEXION
+        public bool Conectar()
+        {
+            bool estado = true;
+            try
+            {
+                Conectar.ConnectionString = cadenaConex;
+                Conectar.Open();
+                
+            }
+            catch (Exception ex)
+            {
+                estado = false;
+                Console.WriteLine("ERROR AL CONECTAR CON EL SERVIDOR");
+            }
+            return estado;
+        }
+        
+        //METODO DESCONECTAR
+        public void Desconectar()
+        {
+            Conectar.Close();
+        }
+
+    }
+}
---
+++ Project1/Project1/ConectarBD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Common;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Project1
+{
+    class ConectarBD
+    {
+       
+
+    }
+}
